d16fed39e2f9ded8194f7b0e8d5ef31f1376292d
public16to <zj@zjdeMacBook-Pro.local>
2020-01-02 19:57:38 +0800

    fix bug

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const route = require('./route');
+
+// node server port
+const PORT = 3000;
+const app = express();
+route.init(app);
+app.use(bodyparser.json());
+app.use(cookieParser());
+
+const httpServer = http.createServer(app);
+
+
+// set index
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+// bind port
+httpServer.listen(PORT, () => {
+  console.log(`http start port:${PORT}`);
+});
---
+++ route.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+// 动态路由
+const loadRoute = {
+  path: './routes/',
+  app: null,
+  // 遍历目录
+  listDir(dir) {
+    const fileList = fs.readdirSync(dir, 'utf-8');
+    for (let i=0; i<fileList.length; i++) {
+      const stat = fs.lstatSync(dir + fileList[i]);
+      // 是目录，需要继续
+      if (stat.isDirectory()) {
+        this.listDir(dir + fileList[i] + '/');
+      } else {
+        this.loadRoute(dir + fileList[i]);
+      }
+    }
+  },
+  // 加载路由
+  loadRoute(routeFile) {
+    const tmpFile = routeFile;
+    const tmp = tmpFile.substring(0, tmpFile.lastIndexOf('.'));
+    const route = require(tmp);
+    // 在路由文件中定义了一个basePath变量，设置路由路径前缀
+    if (route.basePath) {
+      this.app.use(route.basePath, route);
+    } else {
+      this.app.use(route);
+    }
+  },
+  // 初始化入口
+  init(app, newPath) {
+    if (!app) {
+      console.error('系统主参数App未设置');
+      return false;
+    }
+    this.app = app;
+    this.path = newPath || this.path;
+    this.listDir(this.path);
+  },
+};
+
+module.exports = loadRoute;
